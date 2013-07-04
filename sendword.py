dd8e9fad805d0b2d7031aee6429ec953b95c5c8f
James Broadhead <jbroadhead@twitter.com>
2013-07-04 19:26:26 +0200

    Sdfa

--- sendword.py
+++ sendword.py
@@ -5,11 +5,14 @@ from time import sleep
 
 import mosquitto
 
-class Worder:
+class Worder(object):
 
     def __init__(self,):
         with open('/usr/share/dict/words') as fh:
             self.dictionary = fh.readlines()
+
+    def prune_dictionary(self, length):
+        self.dictionary = [ i for i in self.dictionary if len(i) < length ]
 
     def get_word(self,):
         return random.choice(self.dictionary)
